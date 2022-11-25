0886f188428bf9037115ed3d8e3e9173af2f2132
Justin Le Tourneau <5641821+theWizardsBaker@users.noreply.github.com>
2022-11-25 09:50:16 -0600

    updated router history mode

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createMemoryHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createMemoryHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
